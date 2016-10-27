e539a700e5da2f35cdb815b72854f1239e246fc0
jayvpp <jayvpp@gmail.com>
2016-10-27 16:50:42 -0400

    Removing using namespace standard from global namespace

--- source/Time.h
+++ source/Time.h
@@ -1,12 +1,10 @@
 #pragma once
 #include <iostream>
-
-using namespace std;
 class time
 {
 private:
 	int h, m;
-	string to_standard_time()const;
+	std::string to_standard_time()const;
 public:
 	time() { h = 12; m = 1; }
 	time(int h, int m) 
@@ -22,10 +20,6 @@ public:
 	void set_minutes(int minutes);
 	void set_time(int hour, int minutes) { set_hours(hour); set_minutes(minutes); }
 
-	string print_time()const ;
+	std::string print_time()const ;
 };
 
-enum time_period
-{
-	AM, PM
-};
