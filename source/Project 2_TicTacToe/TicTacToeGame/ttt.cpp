425678cc324d49b89482c4a642b3e1870dfb5515
jayvpp <jayvpp@gmail.com>
2016-11-14 18:14:23 -0500

    Forgot to add .h file

--- source/Project 2_TicTacToe/TicTacToeGame/ttt.cpp
+++ source/Project 2_TicTacToe/TicTacToeGame/ttt.cpp
@@ -2,8 +2,24 @@
 
 bool TicTacToe::CheckRow(int row)
 {
-	return (board[row][0] == board[row][1] == board[row][2])
+	return (board[row][0] == board[row][1] == board[row][2]);
 }
+
+bool TicTacToe::CheckCol(int col)
+{
+	return (board[0][col] == board[1][col] == board[2][col]);
+}
+
+bool TicTacToe::CheckDiagonalLeft()
+{
+	return (board[0][0] = board[1][1] = board[2][2]);
+}
+
+bool TicTacToe::CheckDiagonalRight()
+{
+	return (board[0][2] = board[1][1] = board[2][0]);
+}
+
 
 TicTacToe::TicTacToe(Player player1, Player player2)
 {
@@ -15,14 +31,7 @@ TicTacToe::TicTacToe(Player player1, Player player2)
 			board[i][j] = Symbol::Empty;
 }
 
-//Check for winner, if no winner return null
 Player TicTacToe::CheckForWinner()
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		for (size_t j = 0; j < size; j++)
-		{
-		
-		}
-	}
+	
 }
--- source/Project 2_TicTacToe/TicTacToeGame/ttt.h
+++ source/Project 2_TicTacToe/TicTacToeGame/ttt.h
@@ -1,1 +1,47 @@
 #pragma once
+#include <string>
+
+enum Symbol
+{
+	X,
+	O,
+	Empty
+};
+
+
+
+struct Player
+{
+	std::string name;
+	Symbol symbol;
+	int current_score;
+};
+
+struct Position
+{
+	int x;
+	int y;
+};
+
+class TicTacToe
+{
+private:
+	const size_t size = 3;
+	Player player1;
+	Player player2;
+	int currentWinner;
+	Symbol board[3][3];
+	bool CheckRow(int row);
+	bool CheckCol(int col);
+	bool CheckDiagonalLeft();
+	bool CheckDiagonalRight();
+public:
+
+	TicTacToe(Player player1, Player player2);
+	Player CheckForWinner();
+	void ChoosePosition(Position position);
+	void ResetMatch();
+	void ResetGame();
+	void DisplayScore();
+	void DisplayBoard();
+};
